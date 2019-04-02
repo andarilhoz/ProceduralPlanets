9a9473fd06055a94909e47c850cfadf2c4bc0d86
Magno Gouveia <owlflee@gmail.com>
2019-04-02 19:51:21 -0300

    InitialCommit

---
+++ Assets/_scripts/TerrainFace.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.StyleSheets;
+using UnityEngine;
+
+public class TerrainFace
+{
+    private Mesh mesh;
+    private int resolution;
+    private Vector3 localUp;
+    private Vector3 axisA;
+    private Vector3 axisB;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
+    }
+
+    public void ConstructMesh()
+    {
+        var vertices = new Vector3[resolution * resolution];
+        var triangles = new int[resolution - 1 * resolution - 1 * 6];
+        var triIndex = 0;
+
+        for (var y = 0; y < resolution; y++)
+        {
+            for (var x = 0; x < resolution; x++)
+            {
+                var i = x + y * resolution;
+                var percent = new Vector2(x, y) / (resolution - 1);
+                var pointOnCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                vertices[i] = pointOnCube;
+
+                if ( x != resolution - 1 && y != resolution - 1 )
+                {
+                    triangles[triIndex] = i;
+                    triangles[triIndex + 1] = i + resolution + 1;
+                    triangles[triIndex + 2] = i + resolution;
+
+                    triangles[triIndex + 3] = i;
+                    triangles[triIndex + 4] = i + 1;
+                    triangles[triIndex + 5] = i + resolution + 1;
+
+                    triIndex += 6;
+                }
+            }
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
